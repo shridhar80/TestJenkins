9f1d280c77cdc3b808d900266e4b47298088f2ef
shri <patilshridhar8056@gmail.com>
2022-01-22 23:31:27 +0530

    TestJenkins

---
+++ src/test/java/study/TestMain.java
@@ -0,0 +1,14 @@
+package study;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+	 
+	 @Test
+	public void testInputIsEven(){
+	 assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // 
+	//Assertion
+	 }
+	 }
